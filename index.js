554fe89a171ab32110886ef7db8594fa0058fa0f
Conner Gleason <connereg@optonline.net>
2021-08-10 10:11:53 -0400

    commit tree f2bb87ca2eb8d887c046a3525faa33253d0fb0f1

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+
+const returnFirstTwoDrivers = function () {
+    let firstTwoBYIndex = drivers.slice(0, 2)
+    console.log(firstTwoBYIndex)
+    return firstTwoBYIndex
+
+}
+
+const returnLastTwoDrivers = function () {
+    let lastTwoByIndex = drivers.slice(2, 4)
+    console.log(lastTwoByIndex)
+    return lastTwoByIndex
+}
+
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers]);
+
+function createFareMultiplier(multiplier){
+    return function (fare) {
+        return (fare * multiplier)
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2);
+
+
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    function selectDifferentDrivers(drivers){
+        return returnFirstTwoDrivers
+    }
+}
+
+
+
